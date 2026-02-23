c53fa3496e81a69d324b009fa903c5a8effe5801
ebon77 <muhammadshabran77@gmail.com>
2026-02-23 16:59:08 +0700

    view

--- PWL_week_2/routes/web.php
+++ PWL_week_2/routes/web.php
@@ -1,12 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\PhotoController;
 
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
 Route::resource('photos', PhotoController::class);
 Route::get('/', HomeController::class);
 Route::get('/about', AboutController::class);
 Route::get('/articles/{id}', ArticleController::class);
+
+Route::get('/greeting', function () {
+	return view('blog.hello', ['name' => 'Muhammad Shabran']);
+});
