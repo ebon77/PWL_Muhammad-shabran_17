0d989266a835266c9790195f4c178e094c36318d
ebon77 <muhammadshabran77@gmail.com>
2026-03-02 20:55:33 +0700

    Jobsheet 3_Praktiikum 6

--- PWL_week_2_POS/app/Http/Controllers/UserController.php
+++ PWL_week_2_POS/app/Http/Controllers/UserController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-public function showProfile($id, $name) {
-    return view('profile')
-        ->with('id', $id)
-        ->with('name', $name);
-}
+public function index()
+    {
+
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); 
+
+        $user = UserModel::all(); 
+        return view('user', ['data' => $user]);
+    }
 }
--- PWL_week_2_POS/routes/web.php
+++ PWL_week_2_POS/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
-
 
 Route::get('/', function () {
     return view('welcome');
@@ -11,6 +12,7 @@ Route::get('/', function () {
 
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/user', [UserController::class, 'index']);
 
 
 use App\Http\Controllers\HomeController;
@@ -23,10 +25,6 @@ Route::prefix('category')->group(function () {
     Route::get('/home-care', [ProductController::class, 'homeCare']);
     Route::get('/baby-kid', [ProductController::class, 'babyKid']);
 });
-
-
-use App\Http\Controllers\UserController;
-Route::get('/user/{id}/name/{name}', [UserController::class, 'showProfile']);
 
 use App\Http\Controllers\SalesController;
 Route::get('/sales', [SalesController::class, 'index']);
