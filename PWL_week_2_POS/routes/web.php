82bd18d208d383b22ab99fc233f68231b27f1298
ebon77 <muhammadshabran77@gmail.com>
2026-03-02 20:36:09 +0700

    Jobsheet 3_Praktikum 5

--- PWL_week_2_POS/routes/web.php
+++ PWL_week_2_POS/routes/web.php
@@ -1,13 +1,16 @@
 <?php
 
-
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
+use Illuminate\Support\Facades\Route;
+
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
 
 
 use App\Http\Controllers\HomeController;
