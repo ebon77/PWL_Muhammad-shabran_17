e4df0e1bfbb10732ba66b3b45986f81aaf62d884
ebon77 <muhammadshabran77@gmail.com>
2026-04-05 22:30:56 +0700

    Jobsheet5_Praktikum3

--- PWL_week_5/database/seeders/DatabaseSeeder.php
+++ PWL_week_5/database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,6 +22,21 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        Category::create([
+        'name' => 'Web Development',
+        'slug' => 'web-development',
+        ]);
+
+        Category::create([
+        'name' => 'Mobile Programming',
+        'slug' => 'mobile-programming',
+        ]);
+
+        Category::create([
+        'name' => 'UI/UX Design',
+        'slug' => 'ui-ux-design',
         ]);
     }
 }
