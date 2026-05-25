18dd27605add1f3b211c20282f97f6d17c1895b5
ebon77 <muhammadshabran77@gmail.com>
2026-05-25 10:47:38 +0700

    jobsheet 9.1

---
+++ PWL_week_9/app/Filament/Resources/Posts/Tables/PostsTable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ColorColumn;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\IconColumn;
+
+class PostsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                TextColumn::make('title')
+                    ->sortable(),
+                TextColumn::make('slug')
+                    ->sortable(),
+                TextColumn::make('category.name')
+                    ->sortable(),
+                ColorColumn::make('color')
+                    ->sortable(),
+                ImageColumn::make('image')
+                    ->disk('public')
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->label('Created At')
+                    ->dateTime()
+                    ->sortable(),
+                IconColumn::make('published')
+                    ->boolean()
+                    ->sortable(),
+            ])->defaultSort('created_at', 'desc')
+
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+            
+    }
+}
